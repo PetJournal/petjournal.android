dc7a94f88f762e4e850b35700cc1aa2f990ac875
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2023-04-01 00:23:02 -0300

    feature/refactor_method_AlertText

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/util/AlertText.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/util/AlertText.kt
@@ -1,15 +1,19 @@
 package com.soujunior.petjournal.ui.util
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+val paddingForm = Modifier.padding(start = 40.dp, end = 20.dp, top = 2.dp)
 
 @Composable
-fun AlertText(padding: Modifier, textMessage: String){
+fun AlertText(modifier: Modifier = paddingForm, textMessage: String){
     Text(
         text = textMessage,
         color = MaterialTheme.colors.error,
-        modifier = padding
+        modifier = modifier
     )
 }
