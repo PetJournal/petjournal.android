3fdeba635e88e0b6de409458abfa6312c0f305f3
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2023-04-02 18:36:23 -0300

    feature/refactor_Util

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/util/Util.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/util/Util.kt
@@ -1,8 +1,8 @@
 package com.soujunior.petjournal.ui.util
 
 /**
- * isValidLenght = irá retornar um True se o campo de String não estiver Blank, o tamanho da String
- * não for menor que 3 ou maior que 30, e se o campo String não estiver vazio*/
+ * isValidLength = will return True if the String field is not Blank, the length of the String
+ * is not less than 3 or greater than 30, and if the String field is not empty */
 fun isValidLenght(input: String): Boolean {
     return if (input.isNotBlank()) {
         input.length < 3 || input.length > 30 && input.isNotEmpty()
@@ -10,20 +10,29 @@ fun isValidLenght(input: String): Boolean {
 }
 
 /**
- * Se a String de entrada contiver algum caractere que corresponda à expressão regular, a função
- * retornará true, caso contrário, ela retornará false. Portanto, para usar essa função, basta
- * chamar a função e passar a String que você deseja verificar como argumento. O valor de retorno
- * será true se a String contiver algum caractere especial ou número, ou false se não contiver.*/
+ * If the input String contains any characters that match the regular expression, the function
+ * will return true, otherwise it will return false. So, to use this function, just
+ * call the function and pass the String you want to check as an argument. the return value
+ * will be true if the String contains any special characters or numbers, or false if it does not.*/
 fun hasSpecialCharOrNumber(input: String): Boolean {
     val regex = Regex("[^a-zA-Z ]")
     return regex.containsMatchIn(input)
 }
 
+/**
+ * To use the isEmail function, simply call the function and pass the String you want to check as
+ * argument. The return value will be true if the String matches a valid email address, or
+ * false if it doesn't match. Here is the implementation of the isEmail function:*/
 fun isEmail(input: String): Boolean {
     val emailRegex = Regex("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")
     return emailRegex.matches(input)
 }
 
+/**
+ * To use the countCharacters function, just call the function and pass the String you want
+ * check as argument. The return value will be a list of four integers representing
+ * the count of characters in the String. Here is the implementation of the countCharacters function:
+ * */
 fun countCharacters(str: String): List<Int> {
     var digitosMaiusculos = 0
     var digitosMinusculos = 0
