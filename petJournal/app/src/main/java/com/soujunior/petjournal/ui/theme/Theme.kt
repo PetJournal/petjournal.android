e571b20b21792f7f2a747bc4706f609f22ff62f6
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2023-04-07 21:15:28 -0300

    style: update colors and configure theme

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/theme/Theme.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/theme/Theme.kt
@@ -1,69 +1,60 @@
 package com.soujunior.petjournal.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.SideEffect
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
-/*
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+private val LightColors = Colors(
+    primary = light_primary,
+    onPrimary = light_onPrimary,
+    primaryVariant = light_onPrimary,
+    secondary = light_secondary,
+    onSecondary = light_onSecondary,
+    secondaryVariant = light_onSecondary,
+    background = light_background,
+    onBackground = light_onBackground,
+    error = light_error,
+    onError = light_onError,
+    surface = light_surface,
+    onSurface = light_onSurface,
+    isLight = true
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)*/
-
-
-private val LightColorPalette = lightColors(
-    primary = md_theme_light_primary,
-    onPrimary = md_theme_light_onPrimary,
-    secondary = md_theme_light_secondary,
-    onSecondary = md_theme_light_onSecondary,
-    error = md_theme_light_error,
-    onError = md_theme_light_onError,
-    background = md_theme_light_background,
-    onBackground = md_theme_light_onBackground,
-    surface = md_theme_light_surface,
-    onSurface = md_theme_light_onSurface,
+private val DarkColors = Colors(
+    primary = dark_primary,
+    onPrimary = dark_onPrimary,
+    primaryVariant = dark_primaryVariant,
+    secondary = dark_secondary,
+    onSecondary = dark_onSecondary,
+    secondaryVariant = dark_secondaryVariant,
+    error = dark_error,
+    onError = dark_onError,
+    background = dark_background,
+    onBackground = dark_onBackground,
+    surface = dark_surface,
+    onSurface = dark_onSurface,
+    isLight = false
 )
-
-
-private val DarkColors = darkColors(
-    primary = md_theme_dark_primary,
-    onPrimary = md_theme_dark_onPrimary,
-    secondary = md_theme_dark_secondary,
-    onSecondary = md_theme_dark_onSecondary,
-    error = md_theme_dark_error,
-    onError = md_theme_dark_onError,
-    background = md_theme_dark_background,
-    onBackground = md_theme_dark_onBackground,
-    surface = md_theme_dark_surface,
-    onSurface = md_theme_dark_onSurface,
-)
-
-
-
-
 
 @Composable
-fun PetJournalTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColors
-    } else {
-        LightColorPalette
+fun PetJournalTheme(
+    isDynamic: Boolean = false,
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val systemUiController = rememberSystemUiController()
+    val colors = if (darkTheme) DarkColors else LightColors
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = colors.background
+        )
     }
-
     MaterialTheme(
-        typography = Typography,
-        shapes = Shapes,
+        colors = colors,
+        typography = MaterialTheme.typography,
         content = content,
-        colors = colors
     )
 }
