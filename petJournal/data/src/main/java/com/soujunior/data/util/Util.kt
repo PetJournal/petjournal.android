5aa31ce7e42ea40e83136cc727b5f5c304a7c869
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2023-04-16 02:48:20 -0300

    feat: add internet verification

--- petJournal/data/src/main/java/com/soujunior/data/util/Util.kt
+++ petJournal/data/src/main/java/com/soujunior/data/util/Util.kt
@@ -1,4 +1,13 @@
 package com.soujunior.data.util
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+
 object Util {
+   fun statusInternet(context: Context): Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        return activeNetwork?.isConnectedOrConnecting == true
+    }
 }
