815ff266bd74ca7857d42c0e656e6ba73cf81795
Gelson de Souza <76757114+gelsonzeferino@users.noreply.github.com>
2023-05-07 01:41:33 -0300

    test: implementation of unit tests

---
+++ petJournal/app/src/test/java/com/soujunior/petjournal/ui/forgotPasswordScreen/ForgotPassMethodTest.kt
@@ -0,0 +1,90 @@
+package com.soujunior.petjournal.ui.forgotPasswordScreen
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.soujunior.domain.setup.MainCoroutineRule
+import com.soujunior.domain.usecase.auth.ForgotPasswordCase
+import com.soujunior.domain.usecase.base.DataResult
+import com.soujunior.petjournal.setup.formForgot
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.withContext
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.util.concurrent.CountDownLatch
+
+class ForgotPassMethodTest() {
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var coroutineTestRule = MainCoroutineRule()
+
+    private val forgotPasswordCase = mockk<ForgotPasswordCase>(relaxed = true)
+    private val viewModel = ForgotPasswordScreenViewModelImpl(forgotPasswordCase)
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(mainThreadSurrogate)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        mainThreadSurrogate.close()
+    }
+
+    @Test
+    fun `when postForm in register is called, success should be updated correctly`() = runBlocking {
+        coEvery { forgotPasswordCase.execute(formForgot) } returns DataResult.Success("success")
+
+        viewModel.postForm(formForgot)
+
+        val latch = CountDownLatch(1)
+        viewModel.success.observeForever {
+            assertThat(it).isEqualTo("success")
+            latch.countDown()
+        }
+
+        withContext(Dispatchers.IO) {
+            latch.await()
+        }
+    }
+
+
+    @Test
+    fun `when postForm in forgotPassword is called and receive a exception, error should be updated correctly`() = runBlocking {
+        coEvery { forgotPasswordCase.execute(formForgot) } returns DataResult.Failure(Error("error message"))
+
+        viewModel.postForm(formForgot)
+
+        val latch = CountDownLatch(1)
+        viewModel.error.observeForever {
+            assertThat(it).isEqualTo("error message")
+            latch.countDown()
+        }
+
+        withContext(Dispatchers.IO) {
+            latch.await()
+        }
+    }
+}
+
+
--- petJournal/domain/src/main/java/com/soujunior/domain/usecase/auth/ForgotPasswordCase.kt
+++ petJournal/domain/src/main/java/com/soujunior/domain/usecase/auth/ForgotPasswordCase.kt
@@ -14,7 +14,7 @@ class ForgotPasswordCase(
             return if (value != null) {
                 val result = repository.forgotPassword (value)
                 when (result.code) {
-                    in 100..199 -> "O servidor ainda esta processando a requisição"
+                    in 100..199 -> "The server is still processing the request."
                     in 200..299 -> "Success"
                     in 300..399 -> throw Error("Esta faltando alguma dado no formulário")
                     in 400..499 -> throw Error("Erro do Client")
