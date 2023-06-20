66833041bfcb09cfcacee35d3ba141df95834512
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2023-06-20 16:48:35 -0300

    bugFix: refactor code, to fix bug on field email and password

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/accountManager/loginScreen/LoginViewModelImpl.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/accountManager/loginScreen/LoginViewModelImpl.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.viewModelScope
 import com.soujunior.domain.entities.auth.LoginModel
 import com.soujunior.domain.repository.ValidationRepository
 import com.soujunior.domain.usecase.auth.LoginUseCase
+import com.soujunior.domain.usecase.auth.util.ValidationResult
 import com.soujunior.petjournal.ui.ValidationEvent
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
@@ -31,7 +32,6 @@ class LoginViewModelImpl(
             this.error.value = "Erro desconhecido!"
         }
     }
-
     override fun success(resultPostLogin: String) {
         this.success.value = resultPostLogin
         viewModelScope.launch {
@@ -40,28 +40,51 @@ class LoginViewModelImpl(
             validationEventChannel.send(ValidationEvent.Success)
         }
     }
-
     override fun passwordRemember() {}
+    private fun hasError(result: ValidationResult): Boolean {
+        return listOf(result).any { !it.success }
+    }
+    private fun change(
+        email: String? = null,
+        password: String? = null,
+        isRemember: Boolean? = null
+    ) {
+        when {
+            email != null -> {
+                state = state.copy(email = email)
+                val emailResult = validation.validateEmail(state.email)
+                state = if (hasError(emailResult)) state.copy(emailError = emailResult.errorMessage)
+                else state.copy(emailError = null)
+            }
+            password != null -> {
+                state = state.copy(password = password)
+                val passwordResult = validation.validateField(state.password)
+                state =
+                    if (hasError(passwordResult)) state.copy(passwordError = passwordResult.errorMessage)
+                    else state.copy(passwordError = null)
+            }
+            isRemember != null -> {
+                state = state.copy(rememberPassword = isRemember)
+            }
+        }
+    }
     override fun onEvent(event: LoginFormEvent) {
         when (event) {
-            is LoginFormEvent.EmailChanged ->
-                state = state.copy(email = event.email)
-
-            is LoginFormEvent.PasswordChanged ->
-                state = state.copy(password = event.password)
-
-            is LoginFormEvent.RememberPassword ->
-                state = state.copy(rememberPassword = event.isRemember)
-
+            is LoginFormEvent.EmailChanged -> change(email = event.email)
+            is LoginFormEvent.PasswordChanged -> change(password = event.password)
+            is LoginFormEvent.RememberPassword -> change(isRemember = event.isRemember)
             is LoginFormEvent.Submit -> submitData()
         }
     }
-
     override fun submitData() {
         val emailResult = validation.validateEmail(state.email)
-        val hasError = listOf(emailResult).any { !it.success }
+        val passwordResult = validation.validateField(state.password)
+        val hasError = listOf(emailResult, passwordResult).any { !it.success }
         if (hasError) {
-            state = state.copy(emailError = emailResult.errorMessage)
+            state = state.copy(
+                emailError = emailResult.errorMessage,
+                passwordError = passwordResult.errorMessage
+            )
             return
         }
         viewModelScope.launch {
