7a75dd4d40d1484aa4a66da7d77f226ed34783ea
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2023-07-28 09:02:49 -0300

    review: tasks submited

---
+++ petJournal/domain/src/test/java/com/soujunior/domain/usecase/auth/AwaitingCodeUseCaseTest.kt
@@ -0,0 +1,7 @@
+package com.soujunior.domain.usecase.auth
+
+class AwaitingCodeUseCaseTest {
+    /* TODO: Implementas os testes aqui. Mas como pode observar os testes dos outros casos de uso,
+        são bem similares. Não tem problema realizar o CTRL + C CTRL + V, o importante é fazer sentido
+        para o que ela faz ou o que imagina que ela irá fazer posteriormente*/
+}
