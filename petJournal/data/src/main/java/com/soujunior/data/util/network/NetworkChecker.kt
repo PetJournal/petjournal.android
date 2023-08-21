10c380af5e2562379cabc5c2ca456ff3949387cb
Vitor Hilário <meditous@gmail.com>
2023-08-21 15:55:45 -0300

    refactor: moved to data/util/network/

---
+++ petJournal/data/src/main/java/com/soujunior/data/util/network/NetworkChecker.kt
@@ -0,0 +1,20 @@
+package com.soujunior.data.util.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.core.content.ContextCompat.getSystemService
+
+class NetworkChecker(private val context: Context) {
+
+    val isOnline
+        get() = checkIfOnline()
+
+    private fun checkIfOnline(): Boolean {
+        val cm = getSystemService(context, ConnectivityManager::class.java)
+        val capabilities = cm?.getNetworkCapabilities(cm.activeNetwork) ?: return false
+
+        return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+    }
+}
