2a3e240c16f4eefe149baa1de85415d3f8921159
Lucas <oliveira.lucas20@gmail.com>
2023-08-26 12:52:09 -0400

    Remakes species grid screen

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/appArea/speciesChoiceScreen/components/Screen.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/appArea/speciesChoiceScreen/components/Screen.kt
@@ -1,18 +1,26 @@
 package com.soujunior.petjournal.ui.appArea.speciesChoiceScreen.components
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -28,46 +36,55 @@ fun Screen(navController: NavController, userViewModel: UserViewModel) {
     Scaffold(
         bottomBar = { NavigationBar(navController) },
         content = {
-            LazyColumn(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
+            Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-                    //.clip(RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp))
-                    .padding(horizontal = 10.dp)
-                    .background(MaterialTheme.colorScheme.background),
-                content = {
-                    item { Header() }
-                    item { GridVectors() }
-                    item {
-                        Button2(
-                            submit = { /*TODO*/ },
-                            modifier = Modifier.width(180.dp),
-                            enableButton = false,
-                            text = "Outros..."
-                        )
-                    }
-                    item {
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .background(MaterialTheme.colorScheme.background)
+                ) {
+                    Header()
+                    GridVectors()
+                    Spacer(modifier = Modifier.height(5.dp))
+                    Button2(
+                        submit = { /*TODO*/ },
+                        modifier = Modifier.width(180.dp),
+                        enableButton = false,
+                        text = "Outros...",
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Row(
+                        verticalAlignment = Alignment.Bottom
+                    ) {
                         Button2(
                             submit = { /*TODO*/ },
                             modifier = Modifier.width(150.dp),
                             enableButton = true,
-                            text = "Voltar"
+                            border = BorderStroke(width = 2.dp, color = MaterialTheme.colorScheme.primary),
+                            text = "Voltar",
+                            buttonColor = ButtonDefaults.buttonColors(Color.White),
+                            textColor = MaterialTheme.colorScheme.primary
                         )
-                    }
-                    item {
                         Button2(
                             submit = { /*TODO*/ },
                             modifier = Modifier.width(150.dp),
                             enableButton = true,
                             text = "Continuar"
                         )
+
                     }
-                })
+                }
+            }
         }
     )
 }
+
 
 @Preview
 @Composable
