7ca830e6ee3bc4465647b62a7bb3c70399509a41
Lucas <oliveira.lucas20@gmail.com>
2023-08-26 12:51:24 -0400

    Remakes the species grid

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/appArea/speciesChoiceScreen/components/GridVectors.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/appArea/speciesChoiceScreen/components/GridVectors.kt
@@ -1,12 +1,19 @@
 package com.soujunior.petjournal.ui.appArea.speciesChoiceScreen.components
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -20,69 +27,178 @@ fun GridVectors() {
         modifier = Modifier.padding(vertical = 2.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        RoundedSquare(
-            size = calcDefault(defaultSize, 8),
-            topLeftRadius = defaultSize.dp,
-            topRightRadius = defaultSize.dp,
-            bottomLeftRadius = defaultSize.dp,
-            bottomRightRadius = defaultSize.dp,
-            image = painterResource(id = R.drawable.dog),
-            material = MaterialTheme.colorScheme.secondaryContainer,
-        )
-        RoundedSquare(
-            size = calcDefault(defaultSize, 8),
-            topLeftRadius = defaultSize.dp,
-            topRightRadius = defaultSize.dp,
-            bottomLeftRadius = defaultSize.dp,
-            bottomRightRadius = defaultSize.dp,
-            image = painterResource(id = R.drawable.parrot),
-            material = MaterialTheme.colorScheme.secondaryContainer,
-        )
-        RoundedSquare(
-            size = calcDefault(defaultSize, 8),
-            topLeftRadius = defaultSize.dp,
-            topRightRadius = defaultSize.dp,
-            bottomLeftRadius = defaultSize.dp,
-            bottomRightRadius = defaultSize.dp,
-            image = painterResource(id = R.drawable.cat),
-            material = MaterialTheme.colorScheme.secondaryContainer,
-        )
+        Box(
+            modifier = Modifier.border(
+                width = 2.dp,
+                color = Color.Gray,
+                shape = MaterialTheme.shapes.medium
+            ).padding(bottom = 8.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                RoundedSquare(
+                    size = calcDefault(defaultSize, 8),
+                    topLeftRadius = defaultSize.dp,
+                    topRightRadius = defaultSize.dp,
+                    bottomLeftRadius = defaultSize.dp,
+                    bottomRightRadius = defaultSize.dp,
+                    image = painterResource(id = R.drawable.specie_dog),
+                    material = Color.White,
+                )
+                Text(
+                    text = "Cachorro",
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
 
+        Box(
+            modifier = Modifier.border(
+                width = 2.dp,
+                color = Color.Gray,
+                shape = MaterialTheme.shapes.medium
+            ).padding(bottom = 8.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                RoundedSquare(
+                    size = calcDefault(defaultSize, 8),
+                    topLeftRadius = defaultSize.dp,
+                    topRightRadius = defaultSize.dp,
+                    bottomLeftRadius = defaultSize.dp,
+                    bottomRightRadius = defaultSize.dp,
+                    image = painterResource(id = R.drawable.specie_bird),
+                    material = Color.White,
+                )
+                Text(
+                    text = "Pássaro",
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier.border(
+                width = 2.dp,
+                color = Color.Gray,
+                shape = MaterialTheme.shapes.medium
+            ).padding(bottom = 8.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                RoundedSquare(
+                    size = calcDefault(defaultSize, 8),
+                    topLeftRadius = defaultSize.dp,
+                    topRightRadius = defaultSize.dp,
+                    bottomLeftRadius = defaultSize.dp,
+                    bottomRightRadius = defaultSize.dp,
+                    image = painterResource(id = R.drawable.specie_cat),
+                    material = Color.White,
+                )
+                Text(
+                    text = "Gato",
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
     }
+
     Row(
         modifier = Modifier.padding(vertical = 2.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        RoundedSquare(
-            size = calcDefault(defaultSize, 8),
-            topLeftRadius = defaultSize.dp,
-            topRightRadius = defaultSize.dp,
-            bottomLeftRadius = defaultSize.dp,
-            bottomRightRadius = defaultSize.dp,
-            image = painterResource(id = R.drawable.shark),
-            material = MaterialTheme.colorScheme.secondaryContainer,
-        )
-        RoundedSquare(
-            size = calcDefault(defaultSize, 8),
-            topLeftRadius = defaultSize.dp,
-            topRightRadius = defaultSize.dp,
-            bottomLeftRadius = defaultSize.dp,
-            bottomRightRadius = defaultSize.dp,
-            image = painterResource(id = R.drawable.turtle),
-            material = MaterialTheme.colorScheme.secondaryContainer
-        )
-        RoundedSquare(
-            size = calcDefault(defaultSize, 8),
-            topLeftRadius = defaultSize.dp,
-            topRightRadius = defaultSize.dp,
-            bottomLeftRadius = defaultSize.dp,
-            bottomRightRadius = defaultSize.dp,
-            image = painterResource(id = R.drawable.rat),
-            material = MaterialTheme.colorScheme.secondaryContainer,
-        )
+        Box(
+            modifier = Modifier.border(
+                width = 2.dp,
+                color = Color.Gray,
+                shape = MaterialTheme.shapes.medium
+            ).padding(bottom = 8.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                RoundedSquare(
+                    size = calcDefault(defaultSize, 8),
+                    topLeftRadius = defaultSize.dp,
+                    topRightRadius = defaultSize.dp,
+                    bottomLeftRadius = defaultSize.dp,
+                    bottomRightRadius = defaultSize.dp,
+                    image = painterResource(id = R.drawable.specie_fish),
+                    material = Color.White,
+                )
+                Text(
+                    text = "Peixe",
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
 
+        Box(
+            modifier = Modifier.border(
+                width = 2.dp,
+                color = Color.Gray,
+                shape = MaterialTheme.shapes.medium
+            ).padding(bottom = 8.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                RoundedSquare(
+                    size = calcDefault(defaultSize, 8),
+                    topLeftRadius = defaultSize.dp,
+                    topRightRadius = defaultSize.dp,
+                    bottomLeftRadius = defaultSize.dp,
+                    bottomRightRadius = defaultSize.dp,
+                    image = painterResource(id = R.drawable.specie_reptile),
+                    material = Color.White,
+                )
+                Text(
+                    text = "Réptil",
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+        Box(
+            modifier = Modifier.border(
+                width = 2.dp,
+                color = Color.Gray,
+                shape = MaterialTheme.shapes.medium
+            ).padding(bottom = 8.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                RoundedSquare(
+                    size = calcDefault(defaultSize, 8),
+                    topLeftRadius = defaultSize.dp,
+                    topRightRadius = defaultSize.dp,
+                    bottomLeftRadius = defaultSize.dp,
+                    bottomRightRadius = defaultSize.dp,
+                    image = painterResource(id = R.drawable.specie_rodent),
+                    material = Color.White,
+                )
+                Text(
+                    text = "Roedor",
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
     }
-
 }
 
 //// Gustavo isso aqui talvez possa ir pra UTILS e ser reutilizado dps de alguns ajustes.
