380b857b35933856caab47e851051a8cc1747e29
Vitor Hilário <meditous@gmail.com>
2023-09-02 11:58:07 -0300

    refactor: update button to suport loading state

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/components/Button.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/components/Button.kt
@@ -5,13 +5,17 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.soujunior.petjournal.ui.theme.Shapes
 
 /** Componente depreciado, use o Button2*/
@@ -23,7 +27,8 @@ fun Button(
     modifier: Modifier = Modifier,
     text: String = "Button",
     inDarkMode: Boolean,
-    setSystemBarColor: Boolean
+    setSystemBarColor: Boolean,
+    isLoading: Boolean = false
 ) {
 
     Row(
@@ -39,10 +44,17 @@ fun Button(
             border = border,
             shape = Shapes.large
         ) {
-            Text(
-                text = text,
-                style = MaterialTheme.typography.titleSmall,
-            )
+            if (!isLoading) {
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.titleSmall,
+                )
+            } else {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(24.dp),
+                    color = Color.White
+                )
+            }
         }
     }
 }
