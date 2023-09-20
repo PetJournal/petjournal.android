aa6ff68f8384566d2972ae17681e42f7227e4acb
ovitorhilario <meditous@gmail.com>
2023-09-20 12:51:28 -0300

    feat: add email to state model

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/accountManager/awaitingCodeScreen/AwaitingCodeFormEvent.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/accountManager/awaitingCodeScreen/AwaitingCodeFormEvent.kt
@@ -2,5 +2,6 @@ package com.soujunior.petjournal.ui.accountManager.awaitingCodeScreen
 
 sealed class AwaitingCodeFormEvent {
     data class CodeOTPChanged (val code: String) : AwaitingCodeFormEvent()
+    data class EmailChanged (val email: String) : AwaitingCodeFormEvent()
     object Submit : AwaitingCodeFormEvent()
 }
--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/accountManager/awaitingCodeScreen/AwaitingCodeFormState.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/accountManager/awaitingCodeScreen/AwaitingCodeFormState.kt
@@ -3,4 +3,5 @@ package com.soujunior.petjournal.ui.accountManager.awaitingCodeScreen
 data class AwaitingCodeFormState(
     val codeOTP: String = "",
     val codeOTPError: List<String>? = null,
+    val email: String = ""
 )
