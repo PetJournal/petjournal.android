eec095e47f80f707e2dd9bfbeb576dad10721c49
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2023-09-28 10:32:29 -0300

    refactor: extract strings from the login screen to an xml file

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/accountManager/loginScreen/components/Screen.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/accountManager/loginScreen/components/Screen.kt
@@ -17,9 +17,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.soujunior.petjournal.R
 import com.soujunior.petjournal.ui.accountManager.loginScreen.LoginFormEvent
 import com.soujunior.petjournal.ui.accountManager.loginScreen.LoginViewModel
 import com.soujunior.petjournal.ui.components.CheckboxWithText
@@ -35,20 +37,11 @@ fun Screen(navController: NavController, viewModel: LoginViewModel) {
                 .padding(start = 20.dp, end = 20.dp)
                 .align(Alignment.TopCenter)
         ) {
-            item {
-                CreateTitleAndImageLogo(
-                    title = "Acessar conta",
-                    styleTitle = MaterialTheme.typography.displayLarge,
-                    modifierImage = Modifier
-                        .size(width = 200.dp, height = 200.dp)
-                        .padding(top = 20.dp),
-                )
-            }
-            item { Spacer(modifier = Modifier.padding(top = 50.dp)) }
+            item { LoginHeader() }
             item {
                 InputText(
-                    textTop = "Email",
-                    textHint = "eg: exemple@petjournal.com",
+                    textTop = stringResource(id = R.string.email_label),
+                    textHint = stringResource(id = R.string.email_hint),
                     textValue = viewModel.state.email,
                     textError = viewModel.state.emailError,
                     onEvent = { it: String -> viewModel.onEvent(LoginFormEvent.EmailChanged(it)) }
@@ -57,50 +50,15 @@ fun Screen(navController: NavController, viewModel: LoginViewModel) {
             item { Spacer(modifier = Modifier.padding(top = 30.dp)) }
             item {
                 InputText(
-                    textTop = "Senha",
+                    textTop = stringResource(id = R.string.password_label),
                     isPassword = true,
-                    textHint = "Digite sua senha",
+                    textHint = stringResource(id = R.string.password_hint),
                     textValue = viewModel.state.password,
                     textError = viewModel.state.passwordError,
                     onEvent = { it: String -> viewModel.onEvent(LoginFormEvent.PasswordChanged(it)) }
                 )
             }
-            item {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                ) {
-                    Column(
-                        modifier = Modifier.weight(1f)
-                    ) {
-                        CheckboxWithText(
-                            text = "Lembrar",
-                            checkbox = viewModel.state.rememberPassword,
-                            onEvent = { it: Boolean ->
-                                viewModel.onEvent(
-                                    LoginFormEvent.RememberPassword(
-                                        it
-                                    )
-                                )
-                            },
-                        )
-                    }
-                    Text(
-                        text = "Esqueci minha senha",
-                        style = MaterialTheme.typography.bodyLarge,
-                        textAlign = TextAlign.End,
-                        modifier = Modifier
-                            .clickable(
-                                onClick = {
-                                    navController.navigate("forgotPassword")
-                                })
-                            .align(Alignment.CenterVertically),
-                        color = if (isSystemInDarkTheme()) MaterialTheme.colorScheme.primary else Color.Unspecified
-                    )
-                }
-
-            }
+            item { RememberPasswordAndForgotSection(navController, viewModel) }
             item { Spacer(modifier = Modifier.padding(top = 70.dp)) }
             item { Footer(navController, viewModel) }
         }
