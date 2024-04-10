ee014d961b0c4928f3c3e99451b8bab91be00581
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-04-10 10:09:25 -0400

    fix: removed shells from most database class

--- petJournal/database/src/main/java/com/petjournal/database/database/db/AppDatabase.kt
+++ petJournal/database/src/main/java/com/petjournal/database/database/db/AppDatabase.kt
@@ -8,7 +8,7 @@ import com.petjournal.database.database.entity.ApplicationInformation
 import com.petjournal.database.database.entity.GuardianProfile
 import com.petjournal.database.database.entity.PetInformation
 
-@Database(entities = [GuardianProfile::class, ApplicationInformation::class, PetInformation::class]], version = 3, exportSchema = false)
+@Database(entities = [GuardianProfile::class, ApplicationInformation::class, PetInformation::class], version = 3, exportSchema = false)
 
 abstract class AppDatabase : RoomDatabase() {
     abstract fun guardianProfileDao(): GuardianProfileDao
