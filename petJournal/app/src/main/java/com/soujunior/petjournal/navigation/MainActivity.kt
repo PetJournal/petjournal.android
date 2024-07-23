7f0f94184e94c7261e832a8dd8664b4ce3a13103
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-07-23 08:32:05 -0400

    refactor: remove navtest

--- petJournal/app/src/main/java/com/soujunior/petjournal/navigation/MainActivity.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/navigation/MainActivity.kt
@@ -3,9 +3,7 @@ package com.soujunior.petjournal.navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.soujunior.petjournal.ui.theme.PetJournalTheme
 
@@ -14,7 +12,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            TestScreen()
+            PresentationManager()
         }
     }
 }
@@ -45,21 +43,6 @@ fun MainContent() {
         isIntro = false,
         content = {
             NavHostMainContent()
-        }
-    )
-}
-/**
- * Metodo utilizado para testar as telas em desenvolvimento,
- * SEMPRE LEMBRAR DE RETIRAR ESTE METODO DO OnCreate !!!
- **/
-@Preview
-@Composable
-fun TestScreen() {
-    PetJournalTheme(
-        isIntro = false,
-        darkTheme = isSystemInDarkTheme(),
-        content = {
-            NavTestScreen()
         }
     )
 }
