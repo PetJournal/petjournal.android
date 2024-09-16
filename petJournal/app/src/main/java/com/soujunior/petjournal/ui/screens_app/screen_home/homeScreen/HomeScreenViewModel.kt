b66dc2af0c785648123d29ac0170ac301d4066d4
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-09-16 11:21:50 -0400

    feat: add preview in home screen

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/screens_app/screen_home/homeScreen/HomeScreenViewModel.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/screens_app/screen_home/homeScreen/HomeScreenViewModel.kt
@@ -2,15 +2,42 @@ package com.soujunior.petjournal.ui.screens_app.screen_home.homeScreen
 
 
 import androidx.lifecycle.ViewModel
+import com.soujunior.domain.model.PetInformationModel
 import com.soujunior.domain.model.response.GuardianNameResponse
 import com.soujunior.petjournal.R
+import com.soujunior.petjournal.ui.screens_app.screens_pets.petBirthDateScreen.BirthDateFormEvent
+import com.soujunior.petjournal.ui.screens_app.screens_pets.petBirthDateScreen.BirthDateFormState
+import com.soujunior.petjournal.ui.screens_app.screens_pets.petBirthDateScreen.BirthDateViewModel
 import com.soujunior.petjournal.ui.util.ValidationEvent
 import com.soujunior.petjournal.ui.states.TaskState
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.receiveAsFlow
+class FakeHomeViewModel : HomeScreenViewModel() {
+    override var state: HomeState
+        get() = HomeState()
+        set(value) {}
+    override val validationEventChannel = Channel<ValidationEvent>()
+    override val message = MutableStateFlow("Mensagem de Teste")
+    override fun success(name: GuardianNameResponse) {}
 
+    override fun getData() {}
+
+    override fun logout() {}
+
+    override val validationEvents = emptyFlow<ValidationEvent>()
+    override val name: StateFlow<GuardianNameResponse> = MutableStateFlow(GuardianNameResponse("Jorge", ""))
+
+
+    override val taskState = MutableStateFlow<TaskState>(TaskState.Idle)
+
+    override fun failed(exception: Throwable?) { /* No-op */ }
+
+
+}
 abstract class HomeScreenViewModel : ViewModel() {
     abstract val taskState: StateFlow<TaskState>
     abstract var state: HomeState
