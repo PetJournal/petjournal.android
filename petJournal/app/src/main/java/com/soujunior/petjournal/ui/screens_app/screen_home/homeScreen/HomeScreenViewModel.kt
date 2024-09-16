9e4aca8d294bcf07832d4daa886fe539570e26d0
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-09-16 11:22:35 -0400

    refactor: remove imports

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/screens_app/screen_home/homeScreen/HomeScreenViewModel.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/screens_app/screen_home/homeScreen/HomeScreenViewModel.kt
@@ -2,20 +2,17 @@ package com.soujunior.petjournal.ui.screens_app.screen_home.homeScreen
 
 
 import androidx.lifecycle.ViewModel
-import com.soujunior.domain.model.PetInformationModel
 import com.soujunior.domain.model.response.GuardianNameResponse
 import com.soujunior.petjournal.R
-import com.soujunior.petjournal.ui.screens_app.screens_pets.petBirthDateScreen.BirthDateFormEvent
-import com.soujunior.petjournal.ui.screens_app.screens_pets.petBirthDateScreen.BirthDateFormState
-import com.soujunior.petjournal.ui.screens_app.screens_pets.petBirthDateScreen.BirthDateViewModel
-import com.soujunior.petjournal.ui.util.ValidationEvent
 import com.soujunior.petjournal.ui.states.TaskState
+import com.soujunior.petjournal.ui.util.ValidationEvent
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.receiveAsFlow
+
 class FakeHomeViewModel : HomeScreenViewModel() {
     override var state: HomeState
         get() = HomeState()
@@ -29,15 +26,18 @@ class FakeHomeViewModel : HomeScreenViewModel() {
     override fun logout() {}
 
     override val validationEvents = emptyFlow<ValidationEvent>()
-    override val name: StateFlow<GuardianNameResponse> = MutableStateFlow(GuardianNameResponse("Jorge", ""))
+    override val name: StateFlow<GuardianNameResponse> =
+        MutableStateFlow(GuardianNameResponse("Jorge", ""))
 
 
     override val taskState = MutableStateFlow<TaskState>(TaskState.Idle)
 
-    override fun failed(exception: Throwable?) { /* No-op */ }
+    override fun failed(exception: Throwable?) { /* No-op */
+    }
 
 
 }
+
 abstract class HomeScreenViewModel : ViewModel() {
     abstract val taskState: StateFlow<TaskState>
     abstract var state: HomeState
@@ -45,8 +45,8 @@ abstract class HomeScreenViewModel : ViewModel() {
     open val validationEvents: Flow<ValidationEvent>
         get() = validationEventChannel.receiveAsFlow()
 
-    abstract val name : StateFlow<GuardianNameResponse>
-    abstract val message : StateFlow<String>
+    abstract val name: StateFlow<GuardianNameResponse>
+    abstract val message: StateFlow<String>
     abstract fun success(name: GuardianNameResponse)
     abstract fun getData()
     abstract fun logout()
