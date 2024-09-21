11f219a4f7495595f098db38da44f2276991d9f6
Lucas de Oliveira Simão <Oliveira.lucas20@gmail.com>
2024-09-21 13:27:54 -0400

    Renames the directory and test class name to follow the project pattern Creates unit test for the function of removing item from the list

--- petJournal/app/src/test/java/com/soujunior/petjournal/ui/registeredPetScreen/ViewModelRegisteredPetTest.kt
+++ petJournal/app/src/test/java/com/soujunior/petjournal/ui/registeredPetScreen/ViewModelRegisteredPetTest.kt
@@ -1,6 +1,7 @@
-package com.soujunior.petjournal.ui.screens_app.screens_pets.registeredPetScreen
+package com.soujunior.petjournal.ui.registeredPetScreen
 
 import androidx.lifecycle.viewModelScope
+import com.soujunior.domain.model.PetInformationModel
 import com.soujunior.domain.model.response.pet_information.Breed
 import com.soujunior.domain.model.response.pet_information.PetInformationItem
 import com.soujunior.domain.model.response.pet_information.Size
@@ -9,25 +10,27 @@ import com.soujunior.domain.use_case.base.DataResult
 import com.soujunior.domain.use_case.pet.DeletePetInformationUseCase
 import com.soujunior.domain.use_case.pet.GetAllPetInformationUseCase
 import com.soujunior.domain.use_case.util.ValidationRepositoryImpl
+import com.soujunior.petjournal.ui.screens_app.screens_pets.registeredPetScreen.ViewModelRegisteredPetsImpl
 import com.soujunior.petjournal.ui.states.TaskState
+import com.soujunior.petjournal.ui.util.ValidationEvent
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.verify
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
-import java.time.LocalDate
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class ViewModelRegisteredPetsImplTest{
+class ViewModelRegisteredPetTest{
     private lateinit var viewModelTest: ViewModelRegisteredPetsImpl
     private val validation = mockk<ValidationRepositoryImpl>(relaxed = true)
     private val getAllPetInformationUseCase = mockk<GetAllPetInformationUseCase>(relaxed = true)
@@ -84,10 +87,8 @@ class ViewModelRegisteredPetsImplTest{
         coEvery { getAllPetInformationUseCase.execute(Unit) } returns result
 
         viewModelTest.getAllPetInformation()
-        testDispatcher.scheduler.advanceUntilIdle() // Ensure all coroutines have completed
+        testDispatcher.scheduler.advanceUntilIdle()
 
-//        assertEquals(TaskState.Loading, viewModelTest.taskState.value)
-//        coVerify { viewModelTest.success(petList) }
         assertEquals(TaskState.Idle, viewModelTest.taskState.value)
     }
 
@@ -99,11 +100,69 @@ class ViewModelRegisteredPetsImplTest{
         coEvery { getAllPetInformationUseCase.execute(Unit) } returns result
 
         viewModelTest.getAllPetInformation()
-        testDispatcher.scheduler.advanceUntilIdle() // Ensure all coroutines have completed
+        testDispatcher.scheduler.advanceUntilIdle()
 
-//        assertEquals(TaskState.Loading, viewModelTest.taskState.value) // Check that taskState is Loading before failure
-//        coVerify { viewModelTest.failed(error) }
-        assertEquals(TaskState.Idle, viewModelTest.taskState.value) // Check that taskState is Idle after failure
+        assertEquals(TaskState.Idle, viewModelTest.taskState.value)
+    }
+
+    @Test
+    fun `should set TaskState to Loading when deletePetInformation is called`() = runTest {
+        coEvery { deletePetInformationUseCase.execute(1L) } returns DataResult.Success(Unit)
+
+        viewModelTest.deletePetInformation(1L)
+        testDispatcher.scheduler.advanceUntilIdle()
+
+        assertEquals(TaskState.Idle, viewModelTest.taskState.value)
+    }
+
+    @Test
+    fun `should call execute on deletePetInformationUseCase with correct petId`() = runTest {
+        coEvery { deletePetInformationUseCase.execute(1L) } returns DataResult.Success(Unit)
+
+        viewModelTest.deletePetInformation(1L)
+
+        coVerify { deletePetInformationUseCase.execute(1L) }
+    }
+
+    @Test
+    fun `should remove pet from registeredPetList when deletion is successful`() = runTest {
+        val petToDeleteId = 1L
+        viewModelTest.state = viewModelTest.state.copy(
+            registeredPetList = listOf(
+                PetInformationItem(
+                    id = "1",
+                    guardianId = "123",
+                    specie = Specie("1", "Cachorro"),
+                    specieAlias = "Dog",
+                    petName = "Rex",
+                    gender = "Masculino",
+                    breedAlias = "Pastor Alemão",
+                    breed = Breed("1", "Pastor Alemão"),
+                    size = Size("1", "Grande"),
+                    castrated = true,
+                    dateOfBirth = "2018/5/20"
+                ),
+                PetInformationItem(
+                    id = "2",
+                    guardianId = "124",
+                    specie = Specie("2", "Gato"),
+                    specieAlias = "Cat",
+                    petName = "Mimi",
+                    gender = "Feminino",
+                    breedAlias = "Siamês",
+                    breed = Breed("2", "Siamês"),
+                    size = Size("2", "Pequeno"),
+                    castrated = false,
+                    dateOfBirth = "2020/8/15"
+                )
+            )
+        )
+        coEvery { deletePetInformationUseCase.execute(1L) } returns DataResult.Success(Unit)
+
+        viewModelTest.deletePetInformation(petToDeleteId)
+
+        assertEquals(1, viewModelTest.state.registeredPetList.size)
+        assertEquals("2", viewModelTest.state.registeredPetList[0].id)
     }
 
 }
