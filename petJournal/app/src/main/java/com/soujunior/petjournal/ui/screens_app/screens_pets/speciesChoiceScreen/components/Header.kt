3afe88366d924b163d0b95bd819ee919a8d10df6
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-10-26 11:02:37 -0400

    refactor: refactor screen name and gende to new design

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/screens_app/screens_pets/speciesChoiceScreen/components/Header.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/screens_app/screens_pets/speciesChoiceScreen/components/Header.kt
@@ -1,13 +1,9 @@
 package com.soujunior.petjournal.ui.screens_app.screens_pets.speciesChoiceScreen.components
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.soujunior.petjournal.R
 import com.soujunior.petjournal.ui.components.CreateTitleAndImageLogo
 import com.soujunior.petjournal.ui.util.capitalizeFirstLetter
@@ -20,14 +16,14 @@ fun Header(name: String?) {
     if (!name.isNullOrEmpty()) {
         CreateTitleAndImageLogo(
             spaceBetween = 25.sdp,
-            title = stringResource(id = R.string.hello_choice_specie, name.capitalizeFirstLetter()),
+            titleSecundary = stringResource(id = R.string.hello_choice_specie, name.capitalizeFirstLetter()),
             styleTitle = MaterialTheme.typography.headlineLarge,
         )
 
     } else {
         CreateTitleAndImageLogo(
             spaceBetween = 25.sdp,
-            title = stringResource(id = R.string.choice_specie),
+            titleSecundary = stringResource(id = R.string.choice_specie),
             styleTitle = MaterialTheme.typography.headlineLarge,
         )
     }
