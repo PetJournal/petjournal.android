da0b789f60af8a695fc9bcc9d6286ef273d48d48
Lucas de Oliveira Simão <Oliveira.lucas20@gmail.com>
2025-03-22 14:57:43 -0400

    - Updated the font family from Fredoka to Roboto. - Added Roboto font files: bold, light, medium, regular, and semi-bold. - Modified text styles to use the new Roboto font family and adjusted the font sizes and letter spacing.

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/theme/Typography.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/theme/Typography.kt
@@ -7,77 +7,77 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.sp
 import com.soujunior.petjournal.R
 
-val FredokaSemiBold = Font(R.font.fredoka_semi_bold)
-val FredokaRegular = Font(R.font.fredoka_regular)
-val FredokaMedium = Font(R.font.fredoka_medium)
-val FredokaLight = Font(R.font.fredoka_light)
-val FredokaBold = Font(R.font.fredoka_bold)
+val RobotoSemiBold = Font(R.font.roboto_semi_bold)
+val RobotoRegular = Font(R.font.roboto_regular)
+val RobotoMedium = Font(R.font.roboto_medium)
+val RobotoLight = Font(R.font.roboto_light)
+val RobotoBold = Font(R.font.fredoka_bold)
 
 val Typography = Typography(
     displayLarge = TextStyle(
-        fontFamily = FontFamily(FredokaSemiBold),
+        fontFamily = FontFamily(RobotoSemiBold),
         fontSize = 30.sp,
         letterSpacing = 1.5.sp
     ),
     displayMedium = TextStyle(
-        fontFamily = FontFamily(FredokaSemiBold),
+        fontFamily = FontFamily(RobotoSemiBold),
         fontSize = 25.sp,
         letterSpacing = 0.5.sp
     ),
     displaySmall = TextStyle(
-        fontFamily = FontFamily(FredokaSemiBold),
+        fontFamily = FontFamily(RobotoSemiBold),
         fontSize = 20.sp,
         letterSpacing = 0.sp
     ),
     headlineLarge = TextStyle(
-        fontFamily = FontFamily(FredokaMedium),
+        fontFamily = FontFamily(RobotoMedium),
         fontSize = 20.sp,
         letterSpacing = 0.25.sp
     ),
     headlineMedium = TextStyle(
-        fontFamily = FontFamily(FredokaMedium),
+        fontFamily = FontFamily(RobotoMedium),
         fontSize = 10.sp,
         letterSpacing = 0.sp
     ),
     headlineSmall = TextStyle(
-        fontFamily = FontFamily(FredokaMedium),
+        fontFamily = FontFamily(RobotoMedium),
         fontSize = 5.sp,
         letterSpacing = 0.15.sp
     ),
 
     titleLarge = TextStyle(
-        fontFamily = FontFamily(FredokaRegular),
+        fontFamily = FontFamily(RobotoRegular),
         fontSize = 16.sp,
         letterSpacing = 0.15.sp
     ),
     titleMedium = TextStyle(
-        fontFamily = FontFamily(FredokaMedium),
+        fontFamily = FontFamily(RobotoMedium),
         fontSize = 14.sp,
         letterSpacing = 0.1.sp
     ),
 
     bodyLarge = TextStyle(
-        fontFamily = FontFamily(FredokaRegular),
-        fontSize = 16.sp,
+        fontFamily = FontFamily(RobotoRegular),
+        fontSize = 14.sp,
         letterSpacing = 0.5.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = FontFamily(FredokaRegular),
+        fontFamily = FontFamily(RobotoRegular),
         fontSize = 14.sp,
-        letterSpacing = 0.25.sp
+        letterSpacing = 0.15.sp
     ),
     titleSmall = TextStyle(
-        fontFamily = FontFamily(FredokaBold),
+        fontFamily = FontFamily(RobotoBold),
         fontSize = 18.sp,
         letterSpacing = 1.25.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = FontFamily(FredokaLight),
+        fontFamily = FontFamily(RobotoLight),
         fontSize = 12.sp,
         letterSpacing = 0.4.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily(FredokaSemiBold),
+        fontFamily = FontFamily(RobotoSemiBold),
         fontSize = 10.sp,
         letterSpacing = 1.5.sp
     )
