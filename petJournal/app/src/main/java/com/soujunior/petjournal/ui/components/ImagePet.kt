e0996d14273aa252681342e85f0c94dbcead136e
Lucas de Oliveira Simão <Oliveira.lucas20@gmail.com>
2025-03-31 11:42:25 -0400

    - Updated the ImagePet component to:   - Display an image of a pet with rounded corners.   - Include an edit icon with a circular background.   - Include a delete icon.   - Refactored to use `Image` instead of `Icon`.   - Use a `Row` and `Column` for better layout.   - Added `contentScale` to `Image`.   - Changed edit icon to `Image`   - Added `sdp` for size.

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/components/ImagePet.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/components/ImagePet.kt
@@ -1,70 +1,106 @@
 package com.soujunior.petjournal.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.soujunior.petjournal.R
+import ir.kaaveh.sdpcompose.sdp
 
 
 @Composable
 fun ImagePet(
     modifier: Modifier = Modifier,
 ) {
-    Box(
+    Row(
         modifier = modifier
-            .size(150.dp)
-            .clip(RoundedCornerShape(16.dp))
-            .background(Color.White)
-            .border(1.dp, Color.LightGray, RoundedCornerShape(16.dp))
-            .clickable {
-                // TODO: Colocar funcao de abrir galeria e tirar foto
-            },
-        contentAlignment = Alignment.Center
+            .fillMaxWidth()
+            .background(Color.Transparent),
+        horizontalArrangement = Arrangement.Center
     ) {
-        Icon(
-            painter = painterResource(id = R.drawable.logo_black),
-            contentDescription = stringResource(R.string.description_pet_icon),
-            tint = Color.Gray,
-            modifier = Modifier.size(140.dp)
-        )
-        Box(
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(8.dp)
-                .size(40.dp)
-                .clip(CircleShape)
-                .background(MaterialTheme.colorScheme.primary)
-                .clickable { },
-            contentAlignment = Alignment.Center
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_edit),
-                contentDescription = stringResource(R.string.description_edit_icon),
-                tint = Color.White,
-                modifier = Modifier.size(30.dp)
-            )
+        Column(modifier = modifier.weight(2f),
+            horizontalAlignment = Alignment.End) {
+            Box(
+                modifier = modifier
+                    .width(150.dp)
+                    .height(153.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(
+                        color = Color.Transparent,
+                        shape = RoundedCornerShape(size = 17.96.dp)
+                    )
+                    .clickable {
+                        // TODO: Colocar função de abrir galeria e tirar foto
+                    },
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.rectangle_150),
+                    contentDescription = "image description",
+                    contentScale = ContentScale.Crop
+                )
+                Box(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .size(24.dp)
+                        .align(Alignment.BottomEnd)
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.primary)
+                        .clickable { },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        modifier = Modifier.size(14.dp),
+                        painter = painterResource(id = R.drawable.ic_edit),
+                        contentDescription = "edit icon",
+                        contentScale = ContentScale.FillBounds
+                    )
+                }
+            }
+
         }
+        Column (modifier = modifier.weight(1f),
+            horizontalAlignment = Alignment.End){
+            Box(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .padding(end = 24.sdp)
+                    .clickable { },
+            ) {
+                Image(
+                    modifier = Modifier.size(24.dp),
+                    painter = painterResource(id = R.drawable.ic_delete),
+                    contentDescription = "delete icon",
+                    contentScale = ContentScale.FillBounds
+                )
+            }
+        }
+
     }
 }
 
 
-@Preview(showBackground = false, showSystemUi = false, device = "id:pixel_4_xl")
+@Preview(showBackground = true, showSystemUi = false, device = "id:pixel_4_xl")
 @Composable
 fun ImagePetPreview() {
     ImagePet()
