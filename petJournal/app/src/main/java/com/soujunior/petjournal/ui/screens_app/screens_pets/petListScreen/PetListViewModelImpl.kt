ba8fc703bf196d0cca99b1263d4984770bb7e556
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2025-04-04 15:47:33 -0300

    feat: add viewmodel and PetResponse

---
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/screens_app/screens_pets/petListScreen/PetListViewModelImpl.kt
@@ -0,0 +1,37 @@
+package com.soujunior.petjournal.ui.screens_app.screens_pets.petListScreen
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.soujunior.petjournal.ui.states.TaskState
+import com.soujunior.petjournal.ui.util.ValidationEvent
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class PetListViewModelImpl : PetListViewModel() {
+    override val message: StateFlow<String> get() = TODO("Not yet implemented")
+    override val validationEventChannel = Channel<ValidationEvent>()
+    override val validationEvents: Flow<ValidationEvent>
+        get() = super.validationEvents
+
+    private val _taskState: MutableStateFlow<TaskState> = MutableStateFlow(TaskState.Idle)
+    override val taskState: StateFlow<TaskState> = _taskState
+
+    init {
+        _taskState.value = TaskState.Idle
+    }
+
+    override fun success() {
+        viewModelScope.launch { validationEventChannel.send(ValidationEvent.Success) }
+    }
+
+    override fun failed(exception: Throwable?) {
+        exception?.message?.let { Log.e(TAG, it) }
+        viewModelScope.launch {
+            validationEventChannel.send(ValidationEvent.Failed)
+        }
+    }
+}
