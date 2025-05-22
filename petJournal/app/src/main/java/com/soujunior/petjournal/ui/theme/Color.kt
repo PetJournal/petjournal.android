f7aab9d42b2f4643c8e37e7ca2ffc9b7e407be9c
Lucas de Oliveira Simão <Oliveira.lucas20@gmail.com>
2025-05-22 12:50:48 -0400

    feat: add dialog colors
    This commit introduces new color definitions for dialog elements:
    - `color_shadow_dialog`
    - `color_border_dialog`
    - `color_background_button_dialog`

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/theme/Color.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/theme/Color.kt
@@ -88,5 +88,8 @@ object ColorCustom {
     val color_selectable_button_4 = Color(0xFFD03A94)
     val color_selectable_button_5 = Color(0xFFFC5555)
     val color_selectable_button_6 = Color(0xFFB78AF7)
+    val color_shadow_dialog = Color(0x4D000000)
+    val color_border_dialog = Color(0xFF8D4CD2)
+    val color_background_button_dialog = Color(0xFF9C4DFF)
 }
 
