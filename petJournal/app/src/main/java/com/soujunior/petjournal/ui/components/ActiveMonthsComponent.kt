53bb05050e59372ad8862d26383f030dffa16616
Lucas de Oliveira Simão <Oliveira.lucas20@gmail.com>
2025-07-15 09:43:46 -0400

    fix: update ActiveMonthsComponent background color

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/components/ActiveMonthsComponent.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/components/ActiveMonthsComponent.kt
@@ -17,7 +17,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -37,7 +36,7 @@ fun ActiveMonthsComponent(activeMonths: Set<String>) {
     Column(
         modifier = Modifier
             .padding(16.dp)
-            .background(Color.White, shape = RoundedCornerShape(16.dp))
+            .background(MaterialTheme.colorScheme.onPrimary, shape = RoundedCornerShape(16.dp))
             .padding(16.dp)
     ) {
         Text(
