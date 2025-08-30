4a12d3561ccc4a336e8105fce13168281566a425
Lucas de Oliveira Simão <Oliveira.lucas20@gmail.com>
2025-08-30 15:09:10 -0400

    feat: Add unfocused color for WheelTimePicker

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/theme/Color.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/theme/Color.kt
@@ -100,4 +100,5 @@ object ColorCustom {
     val color_placeholder = Color(0xFFCCCCCC)
     val color_text_button_transaction_type = Color(0xFF8dD4CD2)
     val color_border_button_transaction_type = Color(0xFFA2A9B1)
+    val color_unfocused_wheelTimePicker = Color(0xFFDADADA)
 }
