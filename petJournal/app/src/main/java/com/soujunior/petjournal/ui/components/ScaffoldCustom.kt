04b5992067428060be55508f4b58896d1761e8a9
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2025-12-09 00:51:40 -0300

    fix: adjusts screens to prevent them from using screen.kt

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/components/ScaffoldCustom.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/components/ScaffoldCustom.kt
@@ -12,7 +12,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
