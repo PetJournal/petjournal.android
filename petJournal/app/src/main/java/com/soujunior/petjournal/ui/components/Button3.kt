e40f39a60b10170e9950730921285e6836c0b6dc
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2026-01-30 19:07:43 -0300

    fix: corrige um pequeno errinho

--- petJournal/app/src/main/java/com/soujunior/petjournal/ui/components/Button3.kt
+++ petJournal/app/src/main/java/com/soujunior/petjournal/ui/components/Button3.kt
@@ -1,6 +1,10 @@
 package com.soujunior.petjournal.ui.components
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -19,8 +23,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
@@ -47,6 +54,14 @@ fun Button3(
     shape: Shape = RoundedCornerShape(size = 50.dp),
     icon: @Composable (() -> Unit)? = null,
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+    val scale by animateFloatAsState(
+        targetValue = if (isPressed) 0.95f else 1f,
+        animationSpec = tween(durationMillis = 100),
+        label = "clickScale",
+    )
+
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center,
@@ -58,8 +73,10 @@ fun Button3(
         androidx.compose.material3.Button(
             onClick = { submit() },
             enabled = enableButton,
+            interactionSource = interactionSource,
             modifier =
                 modifier
+                    .scale(scale)
                     .width(120.sdp)
                     .shadow(
                         elevation = 15.dp,
