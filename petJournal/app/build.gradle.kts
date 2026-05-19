b2cc6f341b324f3d56920ccf295d53b4a7b02f75
Gustavo Oliveira <42920754+gusoliveira21@users.noreply.github.com>
2026-05-18 21:04:25 -0300

    feat: atualiza gradle file

--- petJournal/app/build.gradle.kts
+++ petJournal/app/build.gradle.kts
@@ -61,14 +61,10 @@ android {
 
     buildTypes {
         release {
-            isDebuggable = true
-
             isMinifyEnabled = true
             isShrinkResources = true
 
-//            signingConfig = signingConfigs.getByName("release")
-
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
 
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
